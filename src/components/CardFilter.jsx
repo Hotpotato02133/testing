a7a4fdddf38a10cb219959381dfde49082d3d479
Efraim James <efraimjamestalucod88@gmail.com>
2024-06-25 09:57:39 +0800

    initial commit

--- src/components/CardFilter.jsx
+++ src/components/CardFilter.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { format } from 'date-fns';
 
 function CardFilter({ filterChange }) {
     const [selectedDate, setSelectedDate] = useState(null);
@@ -11,8 +9,9 @@ function CardFilter({ filterChange }) {
     const handleDateChange = (date) => {
         setSelectedDate(date);
 
-        if(date instanceof Date && !isNaN(date.getTime())) {
-            const formattedDate = date.toISOString();
+        if (date instanceof Date && !isNaN(date.getTime())) {
+            // const formattedDate = format(date, 'yyyy-MM-dd');
+            const formattedDate = format(date, 'dd-MM-yyyy');
             filterChange(formattedDate);
         } else {
             filterChange(null);
@@ -24,59 +23,43 @@ function CardFilter({ filterChange }) {
         filterChange(null);
     };
 
-  return (
-    <div className="filter">
-        <a className='icon' href='#' data-bs-toggle="dropdown">
-            <i className='bi bi-three-dots'></i>
-        </a>
-        <ul className='dropdown-menu dropdown-menu-end dropdown-menu-arrow'>
-            <li className='dropdown-header text-start'>
-                <h6>Filter</h6>
-            </li> 
-            <li>
-                <a className='dropdown-item' onClick={() => filterChange('Today')}>
-                    Today
-                </a>
-            </li>
-            <li>
-                <a className='dropdown-item' onClick={() => filterChange('This week')}>
-                    This Week
-                </a>
-            </li>
-            <li>
-                <a
-                    className='dropdown-item'
-                    onClick={() => filterChange('This Month')}
-                >
-                    This Month
-                </a>
-            </li>
-            <li>
-                <a
-                    className='dropdown-item'
-                    onClick={() => filterChange('This Year')}
-                >
-                    This Year
-                </a>
-            </li>
-            <li>
-                <div className='dropdown-item'>
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DemoContainer components={['DatePicker']}>
-                            <DatePicker
-                                label="Select Date"
-                                value={selectedDate}
-                                onChange={handleDateChange}
-                                clearable
-                                onClear={handleClearDate}
-                            />
-                        </DemoContainer>
-                    </LocalizationProvider>
-                </div>
-            </li>
-        </ul>
-    </div>
-  );
+    return (
+        <div className="filter">
+            <a className="icon" href="#" data-bs-toggle="dropdown">
+                <i className="bi bi-three-dots"></i>
+            </a>
+            <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                <li className="dropdown-header text-start">
+                    <h6>Filter</h6>
+                </li>
+                <li>
+                    <a className="dropdown-item" onClick={() => filterChange('Today')}>
+                        Today
+                    </a>
+                </li>
+                <li>
+                    <a className="dropdown-item" onClick={() => filterChange('This Month')}>
+                        This Month
+                    </a>
+                </li>
+                <li>
+                    <a className="dropdown-item" onClick={() => filterChange('This Year')}>
+                        This Year
+                    </a>
+                </li>
+                <li>
+                    <div className="dropdown-item">
+                        <DatePicker
+                            selected={selectedDate}
+                            onChange={handleDateChange}
+                            isClearable
+                            placeholderText="Select Date"
+                        />
+                    </div>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
-export default CardFilter
+export default CardFilter;
